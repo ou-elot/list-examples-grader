ea6c4ac53fd63b1b5746ad247c89100f9cc1b9e3
ou-elot <146787722+ou-elot@users.noreply.github.com>
2023-11-27 16:50:00 -0800

    Update grade.sh

--- grade.sh
+++ grade.sh
@@ -1,3 +1,4 @@
+
 CPATH='.:lib/hamcrest-core-1.3.jar:lib/junit-4.13.2.jar'
 
 rm -rf student-submission
@@ -14,3 +15,22 @@ echo 'Finished cloning'
 
 # Then, add here code to compile and run, and do any post-processing of the
 # tests
+if ! test -f student-submission/ListExamples.java; then
+  echo "File does not exist."
+fi
+
+cp student-submission/ListExamples.java grading-area
+cp TestListExamples.java grading-area
+
+javac -cp ".;lib/hamcrest-core-1.3.jar;lib/junit-4.13.2.jar" grading-area/*.java
+java -cp ".;lib/junit-4.13.2.jar;lib/hamcrest-core-1.3.jar" org.junit.runner.JUnitCore grading-area/TestListExamples > grading-area/test-results.txt
+
+$stringExists = grep "FAILURES" grading-area/test-results.txt
+
+if [ $stringExists -eq 1 ]
+then 
+    echo "your code did not pass all tests" >> grading-area/test-results.txt
+else 
+    echo "your code passed the tests" >> grading-area/test-results.txt
+    
+fi
